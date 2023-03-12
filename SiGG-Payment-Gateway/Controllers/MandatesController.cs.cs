8d06665e324781ef2d63984caca5a8d777b26355
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-03-12 12:22:39 +0700

    SiGG-Payment-Gateway-Management-Series-Controller-Mandates

---
+++ SiGG-Payment-Gateway/Controllers/MandatesController.cs.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiGG_Payment_Gateway.Controllers
+{
+
+    [Route("api/controller")]
+    [ApiController]
+
+    public class MandatesController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<TransactionsController> _logger;
+        private object txn_date;
+
+        public MandatesController(ILogger<TransactionsController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpPost("v1/Mandates/{id}")]
+        public IEnumerable<Transactions> Post()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Transactions
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                txn_date = Summaries[Random.Shared.Next(Summaries.Length)],
+                mid = Summaries[Random.Shared.Next(Summaries.Length)],
+            })
+            .ToArray();
+        }
+
+        [HttpGet("v1/Mandates")]
+        public IEnumerable<Transactions> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Transactions
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                txn_date = Summaries[Random.Shared.Next(Summaries.Length)],
+                mid = Summaries[Random.Shared.Next(Summaries.Length)],
+            })
+            .ToArray();
+        }
+
+        [HttpPut("v1/Mandates")]
+        public IEnumerable<Transactions> Put()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Transactions
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                txn_date = Summaries[Random.Shared.Next(Summaries.Length)],
+                mid = Summaries[Random.Shared.Next(Summaries.Length)],
+
+
+            })
+            .ToArray();
+        }
+
+        [HttpDelete("v1/Mandates")]
+        public IEnumerable<Transactions> Delete()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Transactions
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                txn_date = Summaries[Random.Shared.Next(Summaries.Length)],
+                mid = Summaries[Random.Shared.Next(Summaries.Length)],
+
+
+            })
+            .ToArray();
+        }
+
+        [HttpPut("v1/Mandates/CollectionSettings/{reference_number}")]
+        public IEnumerable<Transactions> Put2()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Transactions
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                txn_date = Summaries[Random.Shared.Next(Summaries.Length)],
+                mid = Summaries[Random.Shared.Next(Summaries.Length)],
+
+
+            })
+            .ToArray();
+        }
+
+        [HttpPost("v1/Mandates/Collections")]
+        public IEnumerable<Transactions> Post2()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Transactions
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                txn_date = Summaries[Random.Shared.Next(Summaries.Length)],
+                mid = Summaries[Random.Shared.Next(Summaries.Length)],
+            })
+            .ToArray();
+        }
+
+        [HttpGet("v1/Mandates/Collections")]
+        public IEnumerable<Transactions> Get2()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Transactions
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                txn_date = Summaries[Random.Shared.Next(Summaries.Length)],
+                mid = Summaries[Random.Shared.Next(Summaries.Length)],
+            })
+            .ToArray();
+        }
+
+
+        [HttpDelete("v1/Mandates/Collections")]
+        public IEnumerable<Transactions> Delete2()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Transactions
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                txn_date = Summaries[Random.Shared.Next(Summaries.Length)],
+                mid = Summaries[Random.Shared.Next(Summaries.Length)],
+
+
+            })
+            .ToArray();
+        }
+    }
+}
