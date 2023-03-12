88fe0c740a8d43693fca8b7462062dea21de1ee2
SiGGFedCJKp <baannokhooksg@gmail.com>
2023-03-12 13:25:37 +0700

    SiGG-Payment-Gateway-Management-Series-Controller-Partners

---
+++ SiGG-Payment-Gateway/Controllers/PartnersController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiGG_Payment_Gateway.Controllers
+{
+
+    [Route("api/controller")]
+    [ApiController]
+    public class PartnersController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<MandatesController> _logger;
+        private object txn_date;
+
+        public PartnersController(ILogger<PartnersController> logger)
+        {
+            _logger = (ILogger<MandatesController>?)logger;
+        }
+
+
+        [HttpPost("v1/Partners")]
+        public IEnumerable<Transactions> Post()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Transactions
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                txn_date = Summaries[Random.Shared.Next(Summaries.Length)],
+                mid = Summaries[Random.Shared.Next(Summaries.Length)],
+            })
+            .ToArray();
+        }
+
+
+        [HttpGet("v1/Partners/MerchantApplications")]
+        public IEnumerable<Transactions> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Transactions
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                txn_date = Summaries[Random.Shared.Next(Summaries.Length)],
+                mid = Summaries[Random.Shared.Next(Summaries.Length)],
+            })
+            .ToArray();
+        }
+
+
+        [HttpGet("v1/Partners/Accounts")]
+        public IEnumerable<Transactions> Get2()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Transactions
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                txn_date = Summaries[Random.Shared.Next(Summaries.Length)],
+                mid = Summaries[Random.Shared.Next(Summaries.Length)],
+            })
+            .ToArray();
+        }
+    }
+}
